df5416e66d328fdb95320b843ce204319466347c
Loren Velasquez <lm.velasquez12@gmail.com>
2019-01-14 18:02:51 -0600

    Add files via upload

---
+++ Python/csc_401/grocery_buddy.py
@@ -0,0 +1,11 @@
+def grocery_buddy(grocery_item):
+"""
+    Write good doc strings for this function
+""""
+    return grocery_item
+
+print(grocery_buddy('apple'))
+print(grocery_buddy('banana'))
+print(grocery_buddy('candy'))
+
+# grocery_buddy('plums', ['plums', 'candy'])
