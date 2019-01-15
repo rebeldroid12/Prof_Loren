b4572dae6519ab73227387cbf6869f232a047f32
Loren Velasquez <lm.velasquez12@gmail.com>
2019-01-14 18:05:28 -0600

    Update grocery_buddy.py

--- Python/csc_401/grocery_buddy.py
+++ Python/csc_401/grocery_buddy.py
@@ -1,7 +1,7 @@
 def grocery_buddy(grocery_item):
-"""
-    Write good doc strings for this function
-""""
+    """
+        Write good doc strings for this function
+    """
     return grocery_item
 
 print(grocery_buddy('apple'))
